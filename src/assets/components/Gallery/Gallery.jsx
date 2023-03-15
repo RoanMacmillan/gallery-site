ba5fd95185725d29b65886d441c99488a07aede4
RoanMacmillan <hibee94@gmail.com>
2023-03-15 12:53:10 +0000

    added fade in animation to masonry layout

--- src/assets/components/Gallery/Gallery.jsx
+++ src/assets/components/Gallery/Gallery.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Gallery.css";
 import galleryData from "../../data.json";
 
-
 const Gallery = () => {
+  const galleryRef = useRef(null);
+
+  useEffect(() => {
+    const galleryItems = galleryRef.current.querySelectorAll(".galleryItem");
+    let delay = .5;
+    galleryItems.forEach((item) => {
+      item.style.animationDelay = delay + "s";
+      delay += 0.35;
+    });
+  }, []);
+
   return (
     <main>
-      <div className="gallery">
+      <div className="gallery" ref={galleryRef}>
         {galleryData.map((item) => (
           <div className="galleryItem" key={item.name}>
             <div className="gradientWrapper"></div>
             <img src={item.images.thumbnail} alt={item.name} />
             <div className="textContainer">
-            <h2>{item.name}</h2>
-            <p>{item.artist.name}</p>
+              <h2>{item.name}</h2>
+              <p>{item.artist.name}</p>
             </div>
           </div>
         ))}
@@ -23,3 +33,4 @@ const Gallery = () => {
 };
 
 export default Gallery;
+
