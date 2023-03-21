5273d6d874901cbf57afcf438ea3ef870e3d8774
RoanMacmillan <hibee94@gmail.com>
2023-03-21 18:14:03 +0000

    final changes

--- src/assets/components/Slideshow/Slideshow.jsx
+++ src/assets/components/Slideshow/Slideshow.jsx
@@ -1,12 +1,10 @@
-import { useFetcher, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import galleryData from "../../data.json";
 import "./Slideshow.css";
-import React, { useEffect, useState, useRef } from "react";
-import backBtn from "../../images/shared/icon-back-button.svg";
-import fwdBtn from "../../images/shared/icon-next-button.svg";
+import React, { useState, useRef } from "react";
 import enlargeIcon from "../../images/shared/icon-view-image.svg";
-import { CSSTransition, SwitchTransition } from "react-transition-group";
-import ProgressBar from "../ProgressBar/ProgressBar";
+import { CSSTransition } from "react-transition-group";
+import NavigationContainer from "../NavigationContainer/NavigationContainer";
 
 const Slideshow = () => {
   const [showModal, setShowModal] = useState(false);
@@ -36,73 +34,53 @@ const Slideshow = () => {
   const nextImage = () =>
     setCurrentImageIndex((currentImageIndex + 1) % images.length);
 
-  // keeps track of slideshow progress
-  const [progress, setProgress] = useState(0);
-  useEffect(() => {
-    setProgress(((currentImageIndex + 1) / images.length) * 100);
-  }, [currentImageIndex, images]);
-
-
   return (
     <div className="slideShow">
-      <SwitchTransition mode="out-in">
-        <CSSTransition
-          key={currentImageIndex}
-          nodeRef={nodeRef}
-          timeout={0} // Adjust this value to control the duration of the transition
-          classNames="slide"
-          unmountOnExit
-        >
-          <div className="desktopWrapper">
-            <div className="heroTitleWrapper">
-              <img
-                src={images[currentImageIndex].images.hero.small}
-                alt={images[currentImageIndex].name}
-                className="hero"
-              ></img>
-              <img
-                src={images[currentImageIndex].images.hero.large}
-                alt={images[currentImageIndex].name}
-                className="heroLarge"
-              ></img>
+      <div className="desktopWrapper">
+        <div className="heroTitleWrapper">
+          <img
+            src={images[currentImageIndex].images.hero.small}
+            alt={images[currentImageIndex].name}
+            className="hero"
+          ></img>
+          <img
+            src={images[currentImageIndex].images.hero.large}
+            alt={images[currentImageIndex].name}
+            className="heroLarge"
+          ></img>
 
-              <div className="heroBtnWrapper">
-                <button
-                  className="modalBtn"
-                  type="button"
-                  onClick={() => setShowModal(true)}
-                >
-                  <img src={enlargeIcon} alt="View image icon" />
-                  view image
-                </button>
-              </div>
-              <div className="artistLabel">
-                <h1>{images[currentImageIndex].name}</h1>
-                <p>{images[currentImageIndex].artist.name}</p>
-              </div>
-              <img
-                src={images[currentImageIndex].artist.image}
-                alt={images[currentImageIndex].artist.name}
-                className="portrait"
-              />
-            </div>
-
-            <div className="rightContainer">
-              <div className="descripWrapper">
-                <p className="descrip">
-                  {images[currentImageIndex].description}
-                </p>
-                <p className="bigText">{images[currentImageIndex].year}</p>
-              </div>
-              <a className="sourceLink" href={images[currentImageIndex].source}>
-                Go to source
-              </a>
-            </div>
+          <div className="heroBtnWrapper">
+            <button
+              className="modalBtn"
+              type="button"
+              onClick={() => setShowModal(true)}
+            >
+              <img src={enlargeIcon} alt="View image icon" />
+              view image
+            </button>
           </div>
-        </CSSTransition>
-      </SwitchTransition>
+          <div className="artistLabel">
+            <h1>{images[currentImageIndex].name}</h1>
+            <p>{images[currentImageIndex].artist.name}</p>
+          </div>
+          <img
+            src={images[currentImageIndex].artist.image}
+            alt={images[currentImageIndex].artist.name}
+            className="portrait"
+          />
+        </div>
 
-      {/* displays a modal when hero image is clicked */}
+        <div className="rightContainer">
+          <div className="descripWrapper">
+            <p className="descrip">{images[currentImageIndex].description}</p>
+            <p className="bigText">{images[currentImageIndex].year}</p>
+          </div>
+          <a className="sourceLink" href={images[currentImageIndex].source}>
+            Go to source
+          </a>
+        </div>
+      </div>
+
       <CSSTransition
         in={showModal}
         nodeRef={nodeRef}
@@ -110,8 +88,6 @@ const Slideshow = () => {
         classNames="fade"
         unmountOnExit
       >
-        {/* {showModal && ( */}
-        {/* // handles closing modal when clicking outside */}
         <div
           className="modal"
           ref={nodeRef}
@@ -131,28 +107,14 @@ const Slideshow = () => {
             </button>
           </div>
         </div>
-
-        {/* )} */}
       </CSSTransition>
 
-      <div className="navigationContainer">
-        {/* <div className="progressBar" style={{ width: `${progress}%` }}></div> */}
-        <ProgressBar currentImageIndex={currentImageIndex} images={images} />
-        <div className="emptyBar"></div>
-        <div className="navigationTitle">
-          <h2>{images[currentImageIndex].name}</h2>
-          <p>{images[currentImageIndex].artist.name}</p>
-        </div>
-
-        <div className="navigationButtons">
-          <button onClick={prevImage}>
-            <img src={backBtn} alt="back button"></img>
-          </button>
-          <button onClick={nextImage}>
-            <img src={fwdBtn} alt="next button"></img>
-          </button>
-        </div>
-      </div>
+      <NavigationContainer
+        images={images}
+        currentImageIndex={currentImageIndex}
+        onPrevImage={prevImage}
+        onNextImage={nextImage}
+      />
     </div>
   );
 };
